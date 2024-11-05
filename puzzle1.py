128293ecf22452e9bbd826b863d4d7b1f64a5c9b
Nikoloz Tsiklauri <fabolousnick13@gmail.com>
2024-11-05 22:39:25 +0400

    done

---
+++ puzzle1.py
@@ -0,0 +1,29 @@
+Fcounter = 0
+Bcounter = 0
+queue = []
+
+
+x = int(input("Enter the number of people in the queue: "))
+
+
+for i in range(x):
+    people = str(input("Which way is their cap facing? ('F' for forward and 'B' for backward): "))
+    if people == "F":
+        Fcounter += 1
+        queue.append(people)
+    elif people == "B":
+        Bcounter += 1
+        queue.append(people)
+    else:
+        print("Wear a cap and come back!")
+
+
+if Fcounter > Bcounter:
+    for i in range(len(queue)):
+        if queue[i] == "B":
+            print("Person in position", i, "please flip your cap forwards")
+
+if Fcounter < Bcounter:
+    for i in range(len(queue)):
+        if queue[i] == "F":
+            print("Person in position", i, "please flip your cap backwards") 
